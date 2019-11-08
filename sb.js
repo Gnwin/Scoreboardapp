f5fe88bdac40e4bda5cdb8e4cfabbcb7dfe277f5
Godwin Nwachukwu <nwgwin@gmail.com>
2019-11-08 14:21:12 +0100

    added increment function to scoreboard

--- sb.js
+++ sb.js
@@ -1,17 +1,19 @@
 
 let player1 = Number.parseInt(document.querySelector(".player1").innerHTML);
-let player2 = document.querySelector(".player2").innerHTML;
+let player2 = Number.parseInt(document.querySelector(".player2").innerHTML);
 
 let playerOneButton = document.querySelector(".playerOne");
 let playerTwoButton = document.querySelector(".playerTwo");
 
 playerOneButton.addEventListener("click", function(){
-   let val = player1++;
-   document.querySelector(".player1").innerHTML = val;
+   let val1 = player1++;
+   document.querySelector(".player1").innerHTML = val1;
 })
 
 playerTwoButton.addEventListener("click", function(){
-    player2++;
+    let val2 = player2++;
+   document.querySelector(".player2").innerHTML = val2;
 })
 
-console.log(typeof player1);
+let score = document.querySelector("input").value;
+// document.querySelector(".maxscore").innerHTML = score;
